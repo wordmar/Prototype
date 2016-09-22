fe5dc0493f2c24161de38177d6fa65565028ca1a
Hong Lin <dingyue0026@gmail.com>
2016-09-22 14:56:03 +0800

    把storage ignore掉

--- storage/framework/views/a1eb597e0cc33f65cafc19006b9836daa76aa26c.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection('title', 'Result'); ?>
-
-<?php $__env->startSection('content'); ?>
-    <h1>
-        主頁面
-    </h1>
-
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
